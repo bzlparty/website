b536ac2b34098d907e4ac7c2d2b90b8e6844ea3c
Marc Redemske <code@mgred.dev>
2024-02-23 13:20:38 +0100

    fix: remove Auth header

--- internal/docs_download.sh
+++ internal/docs_download.sh
@@ -1,7 +1,6 @@
 curl \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
   -L \
   https://api.github.com/repos/bzlparty/$1/tarball/$2 |\
   tar xvzf - --strip-components=1 --wildcards **/*.md
